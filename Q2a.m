7f53af5a8ce655e11b93857fd1d7cb675d938f7b
Tom <tomcrasset@gmail.com>
2017-11-21 23:59:29 +0100

    Fait graphique + Conversion Fonctions

--- Q2a.m
+++ Q2a.m
@@ -1,5 +1,5 @@
+function [data1Ech, DistKS] = Q2b()
 importData;
-
 
 % On prend 1 echantillon de 20 pays au hasard
 index_echantillon = randsample(100,20,false);
@@ -10,35 +10,46 @@ end
 
 beer_data = echantillon(:,1);
 spirit_data = echantillon(:,2);
+
 % Calcul de la mediane pour la consommation de biere et d'alcool fort
 median_beer = median(beer_data);
 median_spirit = median(spirit_data);
 
 % Calcul de la moyenne pour la consommation de biere et d'alcool fort
 
-mean_beer = mean(beer_data)
-mean_spirit = mean(spirit_data)
+mean_beer = mean(beer_data);
+mean_spirit = mean(spirit_data);
 
 % Calcul de l'ecart-type pour la consommation de biere et d'alcool fort
 
 std_beer = std(beer_data);
 std_spirit = std(spirit_data);
 
-figure
+figure;
 boxplot(beer_data);
 title("Consommation de biere au monde d'un echantillon de 20");
+ylabel("Nombres de canettes");
 
 
-figure
+figure;
 boxplot(spirit_data);
 title("Consommation de spiritueux au monde d'un echantillon de 20");
+ylabel("Nombres de shots");
 
+figure;
 pop = cdfplot(dataset(:,1));
 hold on;
 set(pop,'color','r');
 ech = cdfplot(beer_data);
 set(ech,'color','b');
-title("Frequence cumulee de la consommation de biere au monde d'un echantillon de 20");
+title({'Polygone de frequence cumulee de la consommation de biere';'de la population et d un echantillon de 20'});
+
+legend('show');
+legend('Population','Echantillon');
+legend('Location','southeast');
+legend('boxoff');
+ylabel("Frequence");
+xlabel("Nombres de canettes");
 hold off;
 
 
@@ -49,8 +60,17 @@ hold on;
 set(pop,'color','r');
 ech = cdfplot(spirit_data);
 set(ech,'color','b');
-title("Frequence cumulee de la consommation de spiritueux au monde d'un echantillon de 20");
+legend('show');
+legend('Population','Echantillon');
+legend('Location','southeast');
+legend('boxoff');
+ylabel("Frequence");
+xlabel("Nombres de shots");
+title({'Polygone de frequence cumulee de la consommation d alcool fort';'de la population et d un echantillon de 20'});
 
-[~,~,KS_beer] = kstest2(beer_data,(dataset(:,1)))
-[~,~,KS_spirit] = kstest2(spirit_data,(dataset(:,2)))
+[~,~,KS_beer] = kstest2(beer_data,(dataset(:,1)));
+[~,~,KS_spirit] = kstest2(spirit_data,(dataset(:,2)));
 
+DistKS = [KS_beer,KS_spirit];
+data1Ech = [median_beer,median_spirit,mean_beer,mean_spirit,std_beer,std_spirit];
+end
--- Q2b.m
+++ Q2b.m
@@ -1,3 +1,6 @@
+function [meanOfAll, data100Ech, DistKS] = Q2b()
+
+
 importData;
 
 echantillons = zeros(20,4,100);
@@ -16,7 +19,6 @@ KS_pure_100  = zeros(100,1);
 % On prend 100 echantillons de 20 pays au hasard
 for j = 1:100
     index_echantillon = randsample(100,20,false);
-    echantillon = zeros(20,4);
     for i = 1:20
         echantillons(i,1:4,j) = dataset(index_echantillon(i),:);
     end
@@ -45,49 +47,63 @@ for j = 1:100
     std_beer_100(j) = std(beer_data);
     std_spirit_100(j) = std(spirit_data);
     
+    
+
+    % Calcul de la distance de Kolmogorov-Smirnov entre la population et
+    % l echantillon pour toutes les boissons
     [~,~,KS_beer] = kstest2(beer_data,(dataset(:,1)));
-    KS_beer_100(j) = KS_beer
+    KS_beer_100(j) = KS_beer;
     [~,~,KS_spirit] = kstest2(spirit_data,(dataset(:,2)));
-    KS_spirit_100(j) = KS_spirit
+    KS_spirit_100(j) = KS_spirit;
     
     [~,~,KS_wine] = kstest2(wine_data,(dataset(:,3)));
-    KS_wine_100(j) = KS_wine
+    KS_wine_100(j) = KS_wine;
     [~,~,KS_pure] = kstest2(pure_data,(dataset(:,4)));
-    KS_pure_100(j) = KS_pure
-
+    KS_pure_100(j) = KS_pure;
 
 end
 
 %Histogrammes
 figure
-median_biere_histogram = histogram(median_beer_100,20);
-title("Median beer 100 echantillon");
+histogram(median_beer_100,0:10:400);
+title({'Histogramme de la repartition des medianes de la ';'consommation de bieres sur 100 echantillons'});
+ylabel("Nombres de medianes");
+xlabel("Nombres de canettes");
 
 figure
-median_spirit_histogram = histogram(median_spirit_100,20);
-title("Median spirit 100 echantillon");
+histogram(median_spirit_100,0:10:450);
+title({'Histogramme de la repartition des medianes de la ';'consommation d alcool fort sur 100 echantillons'});
+ylabel("Nombres de medianes");
+xlabel("Nombres de shots");
 
 figure
-mean_biere_histogram = histogram(mean_beer_100,20);
-title("Mean beer 100 echantillon");
+histogram(mean_beer_100,0:10:400);
+title({'Histogramme de la repartition des moyennes de la ';'consommation de bieres sur 100 echantillons'});
+ylabel("Nombres de medianes");
+xlabel("Nombres de canettes");
+figure
+histogram(mean_spirit_100,0:10:450);
+title({'Histogramme de la repartition des moyennes de la ';'consommation d alcool fort sur 100 echantillons'});
+ylabel("Nombres de moyennes");
+xlabel("Nombres de shots");
 
 figure
-mean_spirit_histogram = histogram(mean_spirit_100,20);
-title("Mean spirit 100 echantillon");
+histogram(std_beer_100,0:10:400);
+title({'Histogramme de la repartition de l ecart-type de la ';'consommation de bieres sur 100 echantillons'});
+ylabel("Nombres d ecart-type");
+xlabel("Nombres de canettes");
 
 figure
-std_biere_histogram = histogram(std_beer_100,20);
-title("Std beer 100 echantillon");
-
-figure
-std_spirit_histogram = histogram(std_spirit_100,20);
-title("Std spirit 100 echantillon");
+histogram(std_spirit_100,0:10:450);
+title({'Histogramme de la repartition de l ecart-type de la ';'consommation d alcool fort sur 100 echantillons'});
+ylabel("Nombres d ecart-type");
+xlabel("Nombres de shots");
 
 
 
 % Calcul de la moyenne de la mediane des echantillons
-mean_median_beer = mean(median_beer_100)
-mean_median_spirit = mean(median_spirit_100)
+mean_median_beer = mean(median_beer_100);
+mean_median_spirit = mean(median_spirit_100);
 
 % Calcul de la moyenne de la moyenne des echantillons
 
@@ -100,34 +116,35 @@ mean_std_beer = mean(std_beer_100);
 mean_std_spirit = mean(std_spirit_100);
 
 
-%Calcul de la distance de Kolmogorov
-
-%[~,~,KS_beer] = kstest2(beer_data,population)
-%[~,~,KS_spirit] = kstest2(spirit_data,population)
 
 
 figure
-histogram(KS_beer_100, 21)
-title('Histogramme des distances de Kolmogrorov pour la bière')
-ylabel('Nombre d echantillons')
-xlabel('Distance de Kolomogorov')
+histogram(KS_beer_100,0:0.025:1)
+title({'Histogramme de la repartition des distances de Kolmogorov-Smirnov';' de la consommation de bieres entre 100 echantillons et la population'});
+ylabel('Nombre de distances de K-S');
+xlabel('Distance de Kolmogorov-Smirnov');
 
 figure
-histogram(KS_spirit_100, 21)
-title('Histogramme des distances de Kolmogrorov pour les spiritueux')
-ylabel('Nombre d echantillons')
-xlabel('Distance de Kolomogorov')
+histogram(KS_spirit_100, 0:0.025:1)
+title({'Histogramme de la repartition des distances de Kolmogorov-Smirnov';' de la consommation d alcool fort entre 100 echantillons et la population'});
+ylabel('Nombre de distances de K-S');
+xlabel('Distance de Kolmogorov-Smirnov');
 
 figure
-histogram(KS_wine_100, 21)
-title('Histogramme des distances de Kolmogrorov pour le vin')
-ylabel('Nombre d echantillons')
-xlabel('Distance de Kolomogorov')
+histogram(KS_wine_100, 0:0.025:1)
+title({'Histogramme de la repartition des distances de Kolmogorov-Smirnov';' de la consommation de vin entre 100 echantillons et la population'});
+ylabel('Nombre de distances de K-S');
+xlabel('Distance de Kolmogorov-Smirnov');
 
 figure
-histogram(KS_pure_100, 21)
-title('Histogramme des distances de Kolmogrorov pour l alcool pur')
-ylabel('Nombre d echantillons')
-xlabel('Distance de Kolomogorov')
+histogram(KS_pure_100, 0:0.025:1)
+title({'Histogramme de la repartition des distances de Kolmogorov-Smirnov';' de la consommation d alcool pur entre 100 echantillons et la population'});
+ylabel('Nombre de distances de K-S');
+xlabel('Distance de Kolmogorov-Smirnov');
+
+meanOfAll = [mean_median_beer,mean_median_spirit,mean_mean_beer,mean_mean_spirit,mean_std_beer,mean_std_spirit];
+data100Ech = [median_beer_100,median_spirit_100,mean_beer_100,mean_spirit_100,std_beer_100,std_spirit_100];
+DistKS = [KS_beer_100,KS_spirit_100,KS_wine_100,KS_pure_100];
 
 
+end
